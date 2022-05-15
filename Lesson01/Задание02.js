8ef9888f985efb2cd4e2c755543e58bcc41f0f94
lephyrina <105231101+lephyrina@users.noreply.github.com>
2022-05-15 21:12:01 +0300

    Lesson02 (#2)
    * Задание 2.1
    
    * Исправление
    
    * Задание к уроку 1 по основам программирования
    
    * Задание к уроку 1 по основам программирования Исправление

---
+++ Lesson01/Задание02.js
@@ -0,0 +1,5 @@
+drawCircle(250, 200, 100, 'black'); // Рисуем смайлику лицо
+drawCircle(200, 180, 20, 'white'); // Правый глаз
+drawCircle(300, 180, 20, 'white'); // Левый глаз
+drawCircle(200, 180, 5, 'black'); // Правый зрачок
+drawCircle(300, 180, 5, 'black'); // Левый зрачок
